bfc4aec41269564b39908c5a10aaf0ea50686d5d
Donovanbkf <oscarbkf1@gmail.com>
2023-04-12 17:09:58 +0200

    validators·

--- src/validators/asignatura.js
+++ src/validators/asignatura.js
@@ -5,14 +5,6 @@ const pool = require('../config/database')
 const validateAsignaturaCreate = [
   body("description").exists().withMessage("description no recibido").notEmpty().withMessage("description vacío"),
   body("skill").isIn(['mates', 'castellano', 'ingles', 'valenciano', 'fisica', 'quimica', 'informatica']).exists().withMessage('skill no recibido'),
-  body("user_id").exists().withMessage("user_id no recibido").notEmpty().withMessage("user_id vacío").isInt().custom(async (value, {req}) => {
-    const user = await pool.query('Select * from user where id = ?', [value]);
-    if (user.length === 0) {
-      req.status = 404
-      throw new Error(`no existe este user`);
-    }
-    return true;
-  }),
   (req, res, next) => {
     validateResult(req, res, next);
   },
@@ -21,22 +13,17 @@ const validateAsignaturaCreate = [
 const validateAsignaturaEdit = [
   body("description").exists().withMessage("description no recibido").notEmpty().withMessage("description vacío"),
   body("skill").isIn(['mates', 'castellano', 'ingles', 'valenciano', 'fisica', 'quimica', 'informatica']).exists().withMessage('skill no recibido'),
-  body("user_id").exists().withMessage("user_id no recibido").notEmpty().withMessage("user_id vacío").isInt().custom(async (value, {req}) => {
-    const user = await pool.query('Select * from user where id = ?', [value]);
-    if (user.length === 0) {
-      req.status = 404
-      throw new Error(`no existe este user`);
-    }
-    return true;
-  }),
   param("id").exists().withMessage("id no recibido").notEmpty().withMessage("id vacío").custom(async (value, {req}) => {
     const asignatura = await pool.query('Select * from asignatura where id = ?', [value]);
     if (asignatura.length == 0) {
       req.status = 404
-      throw new Error(`Request actual no existe`);
+      throw new Error(`Asignatura actual no existe`);
+    }else if (asignatura.user_id != req.user.id){
+      req.status = 403
+      throw new Error(`asignatura actual no te pertenece`);
     }
     return true;
-  }),
+  }), 
   (req, res, next) => {
     validateResult(req, res, next);
   },
@@ -47,7 +34,10 @@ const validateAsignaturaDelete = [
     const asignatura = await pool.query('Select * from asignatura where id = ?', [value]);
     if (asignatura.length == 0) {
       req.status = 404
-      throw new Error(`Request actual no existe`);
+      throw new Error(`Asignatura actual no existe`);
+    }else if (asignatura.user_id != req.user.id){
+      req.status = 403
+      throw new Error(`asignatura actual no te pertenece`);
     }
     return true;
   }),
--- src/validators/matricula.js
+++ src/validators/matricula.js
@@ -5,17 +5,9 @@ const pool = require('../config/database')
 const validateMatriculaCreate = [
   body("nota").exists().withMessage("nota no recibido").notEmpty().withMessage("nota vacío").isFloat().withMessage("tiene que ser float"),
   body("estado").isIn(['en curso', 'finalizada']).exists().withMessage('skill no recibido'),
-  body("user_id").exists().withMessage("user_id no recibido").notEmpty().withMessage("user_id vacío").isInt().custom(async (value, {req}) => {
-    const user = await pool.query('Select * from user where id = ?', [value]);
-    if (user.length === 0) {
-      req.status = 404
-      throw new Error(`no existe este user`);
-    }
-    return true;
-  }),
   body("asignature_id").exists().withMessage("asignature_id no recibido").notEmpty().withMessage("asignature_id vacío").isInt().custom(async (value, {req}) => {
-    const user = await pool.query('Select * from asignature where id = ?', [value]);
-    if (user.length === 0) {
+    const asignatura = await pool.query('Select * from asignature where id = ?', [value]);
+    if (asignatura.length === 0) {
       req.status = 404
       throw new Error(`no existe esta asignatura`);
     }
@@ -29,14 +21,6 @@ const validateMatriculaCreate = [
 const validateMatriculaEdit = [
   body("nota").exists().withMessage("nota no recibido").notEmpty().withMessage("nota vacío").isFloat().withMessage("tiene que ser float"),
   body("estado").isIn(['en curso', 'finalizada']).exists().withMessage('skill no recibido'),
-  body("user_id").exists().withMessage("user_id no recibido").notEmpty().withMessage("user_id vacío").isInt().custom(async (value, {req}) => {
-    const user = await pool.query('Select * from user where id = ?', [value]);
-    if (user.length === 0) {
-      req.status = 404
-      throw new Error(`no existe este user`);
-    }
-    return true;
-  }),
   body("asignature_id").exists().withMessage("asignature_id no recibido").notEmpty().withMessage("asignature_id vacío").isInt().custom(async (value, {req}) => {
     const user = await pool.query('Select * from asignature where id = ?', [value]);
     if (user.length === 0) {
@@ -45,6 +29,17 @@ const validateMatriculaEdit = [
     }
     return true;
   }),
+  param("id").exists().withMessage("id no recibido").notEmpty().withMessage("id vacío").custom(async (value, {req}) => {
+    const matricula = await pool.query('Select * from matricula where id = ?', [value]);
+    if (matricula.length == 0) {
+      req.status = 404
+      throw new Error(`Matricula actual no existe`);
+    }else if (matricula.user_id != req.user.id){
+      req.status = 403
+      throw new Error(`Matricula actual no te pertenece`);
+    }
+    return true;
+  }), 
   (req, res, next) => {
     validateResult(req, res, next);
   },
@@ -52,10 +47,13 @@ const validateMatriculaEdit = [
 
 const validateMatriculaDelete = [
   param("id").exists().withMessage("id no recibido").notEmpty().withMessage("id vacío").custom(async (value, {req}) => {
-    const asignatura = await pool.query('Select * from matricula where id = ?', [value]);
-    if (asignatura.length == 0) {
+    const matricula = await pool.query('Select * from matricula where id = ?', [value]);
+    if (matricula.length == 0) {
       req.status = 404
       throw new Error(`matricula actual no existe`);
+    }else if (matricula.user_id != req.user.id){
+      req.status = 403
+      throw new Error(`matricula actual no te pertenece`);
     }
     return true;
   }),
