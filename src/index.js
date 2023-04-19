b392a48220065601e5f5e17b457e9ff6c024da50
Donovanbkf <oscarbkf1@gmail.com>
2023-04-19 21:21:16 +0200

    indexxs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const morgan = require('morgan')
 const express = require('express')
-const pool = require('./config/database') // ahora se ejecuta el poll y debería ssalir db is connected
+const session = require('express-session');
+const passport = require('passport');
+require("./config/passport")(passport);
 
 const app = express()
 
@@ -16,10 +18,22 @@ app.use(morgan('dev'))
 app.use(express.json()) // para postman JSON hace que funcione ¿?
 app.use(express.urlencoded({ extended: true})) // para los form hace que funcione ¿? y son true elimina el [Object: null prototype] { } 
 
+//session
+app.set('trust proxy', 1)
+app.use(session({
+    key: 'classroom',//nombre de la cookie
+    secret: 'secretitio', // Clave secreta para la sesión
+    resave: false, // No guardar sesión si no hay cambios
+    saveUninitialized: false, // No guardar sesión si no hay datos
+    cookie: { maxAge: 3600000 }
+    // store: new mysqlStore(key)   //donde guardar session (bbdd o memoria servidor)
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 //routes
 app.use('/auth',require('./routes/auth')) 
 app.use('/asignatura',require('./routes/asignatura'))
 app.use('/matricula',require('./routes/matricula'))
 app.use(require('./routes/')) 
-
-// app.use(require('./routes/links'))
